a40358444f9625f1550cd50d03ad06c5120e398f
root <root@DESKTOP-38EOAGF.localdomain>
2018-09-16 20:59:55 +0300

    kodikopoihsh

--- encasc85/encasc85.c
+++ encasc85/encasc85.c
@@ -11,38 +11,45 @@ int main(void){
   //l=metraei tis metavlhtes tu pinaka p.
   //N=o arithmos ton 0 pu xreazomaste ga na symplhrothei h tetrada.
   //m=mh proshmasmenos akeraios gai thn kodkopoihsh.
-  bool flag=0,flag2=0;
-  int i=0,j=3,l=0,N=0;
+  bool flag=0;
+  int i=0,j=3,l=0,N=0,r=0;
   unsigned int a=0;
   char c=0,p[5];
   printf("<~");
-  while(i<4){
-    if(flag==0){
-      c=getchar();
-      flag=c==10;//ta ubuntu mu anti gia EOF evazan ton arthmo 10 sto telos tu arxeiu, mu phre poly ora na to katalavo, de gnorizo giati...
+  while(1){
+    while(r<50){
+      i=0,a=0,j=3,N=0;
+      while(i<4){
+        if(flag==0){
+          c=getchar();
+          flag=c==10;//ta ubuntu mu anti gia EOF evazan ton arthmo 10 sto telos tu arxeiu, mu phre poly ora na to katalavo, de gnorizo giati...
+        }
+        if(flag){
+          c=0;
+          N++;
+        }
+        a+=c*pow(256,j);
+        i++;
+        j--;
+      }
+      if(!a && !flag){//ama dhladh exei eisaxthei mia terada apo mdenika bytes, xoris omos na exei ginei epeidh de symplhronotan mia tetrada.
+        putchar('z');
+      }
+      while(a){
+        p[l]=(a%85)+33;
+        a/=85;
+        l++;
+      }
+      for(l--; l>=N; l--){
+        putchar(p[l]);
+      }
+      if(flag){
+        printf("~>\n");
+        exit(0);//afto edo to exo gia argotera pu tha to valo se megalytero loop.
+      }
+      r++;
     }
-    if(flag){
-      c=0;
-      N++;
-    }
-    a+=c*pow(256,j);
-    i++;
-    j--;
-  }
-  if(!a){//ama dhladh exei eisaxthei mia terada apo mdenika bytes.
-    putchar('z');
-  }
-  while(a!=0){
-    p[l]=(a%85)+33;
-    a/=85;
-    l++;
-  }
-  for(l--; l>=N; l--){
-    putchar(p[l]);
-  }
-  if(flag){
-    printf("~>\n");
-    exit(0);//afto edo to exo gia argotera pu tha to valo se megalytero loop.
+    putchar('\n');
   }
   printf("~>\n");
   return 0;
