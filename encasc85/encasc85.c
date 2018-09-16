284738afafaaa53164c628b333acf1bb67ffcb92
root <root@DESKTOP-38EOAGF.localdomain>
2018-09-16 18:38:41 +0300

    tora exei kai gia ta mhdenika bytes

--- encasc85/encasc85.c
+++ encasc85/encasc85.c
@@ -11,7 +11,7 @@ int main(void){
   //l=metraei tis metavlhtes tu pinaka p.
   //N=o arithmos ton 0 pu xreazomaste ga na symplhrothei h tetrada.
   //m=mh proshmasmenos akeraios gai thn kodkopoihsh.
-  bool flag=0;
+  bool flag=0,flag2=0;
   int i=0,j=3,l=0,N=0;
   unsigned int a=0;
   char c=0,p[5];
@@ -28,6 +28,9 @@ int main(void){
     a+=c*pow(256,j);
     i++;
     j--;
+  }
+  if(!a){//ama dhladh exei eisaxthei mia terada apo mdenika bytes.
+    putchar('z');
   }
   while(a!=0){
     p[l]=(a%85)+33;
